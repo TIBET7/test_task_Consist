e8518ed3db570e1ae7b4a208e6cfa1c7c6a6d7a9
Dim <om1234777@gmail.com>
2020-10-10 00:33:06 +0300

    add elements content to editor fields

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,10 @@
 
 const mainCheckbox = document.querySelector('.check__input--main');
 const checkboxes = document.querySelectorAll('.check__input--element');
+const elements = document.querySelectorAll('.elements__item');
+const nameInput = document.querySelector('.editor__element-name');
+const descriptionInput = document.querySelector('.editor__element-description');
+
 
 mainCheckbox.addEventListener('click', () => {
   checkboxes.forEach((checkbox) => {
@@ -13,3 +17,24 @@ mainCheckbox.addEventListener('click', () => {
     }
   });
 });
+
+elements.forEach((element) => {
+  element.addEventListener('click', (event) => {
+    for (let i = 0; i < elements.length; i += 1) {
+      elements[i].classList.remove('active');
+    }
+    // console.log(event.currentTarget);
+    // event.currentTarget.classList.remove('hidden');
+    // event.currentTarget.classList.remove('visible');
+    event.currentTarget.classList.add('active');
+    //console.log(event.currentTarget.children);
+    const elementChildren = event.currentTarget.children;
+    // console.log(elementChildren[1].textContent);
+    // console.log(elementChildren[2].textContent);
+    //nameInput.value = 588;
+    nameInput.value = elementChildren[1].textContent;
+    console.log(nameInput.textContent);
+    descriptionInput.value = elementChildren[2].textContent;
+  });
+});
+
